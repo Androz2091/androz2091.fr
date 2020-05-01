260339ea78e691561ba6efa1b43ec7171bc6351b
Androz2091 <androz2091@gmail.com>
2020-05-01 13:56:04 +0200

    :package: Add boostrap to dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
